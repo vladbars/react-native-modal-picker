ccab8202acf7d3a93594af3857d27b39cac8f423
vladbars <vladbars@gmail.com>
2017-03-17 09:46:55 +0300

    container height

--- style.js
+++ style.js
@@ -8,77 +8,77 @@ const PADDING = 8;
 const BORDER_RADIUS = 5;
 const FONT_SIZE = 16;
 const HIGHLIGHT_COLOR = 'rgba(0,118,255,0.9)';
-const OPTION_CONTAINER_HEIGHT = 400;
+const OPTION_CONTAINER_HEIGHT = height / 2;
 
 export default StyleSheet.create({
 
-    overlayStyle: {
-        width: width,
-        height: height,
-        backgroundColor: 'rgba(0,0,0,0.7)'
-    },
+  overlayStyle: {
+    width: width,
+    height: height,
+    backgroundColor: 'rgba(0,0,0,0.7)'
+  },
 
-    optionContainer: {
-        borderRadius:BORDER_RADIUS,
-        width:width*0.8,
-        height:OPTION_CONTAINER_HEIGHT,
-        backgroundColor:'rgba(255,255,255,0.8)',
-        left:width*0.1,
-        top:(height-OPTION_CONTAINER_HEIGHT)/2
-    },
+  optionContainer: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.8,
+    height: OPTION_CONTAINER_HEIGHT,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    left: width * 0.1,
+    top: (height - OPTION_CONTAINER_HEIGHT) / 2
+  },
 
-    cancelContainer: {
-        left:width*0.1,
-        top:(height-OPTION_CONTAINER_HEIGHT)/2 + 10
-    },
+  cancelContainer: {
+    left: width * 0.1,
+    top: (height - OPTION_CONTAINER_HEIGHT) / 2 + 10
+  },
 
-    selectStyle: {
-        flex: 1,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        padding: 8,
-        borderRadius: BORDER_RADIUS
-    },
+  selectStyle: {
+    flex: 1,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: BORDER_RADIUS
+  },
 
-    selectTextStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  selectTextStyle: {
+    textAlign: 'center',
+    color: '#333',
+    fontSize: FONT_SIZE
+  },
 
-    cancelStyle: {
-        borderRadius: BORDER_RADIUS,
-        width: width * 0.8,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        padding: PADDING
-    },
+  cancelStyle: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.8,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    padding: PADDING
+  },
 
-    cancelTextStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  cancelTextStyle: {
+    textAlign: 'center',
+    color: '#333',
+    fontSize: FONT_SIZE
+  },
 
-    optionStyle: {
-        padding: PADDING,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
+  optionStyle: {
+    padding: PADDING,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
 
-    optionTextStyle: {
-        textAlign: 'center',
-        fontSize: FONT_SIZE,
-        color: HIGHLIGHT_COLOR
-    },
+  optionTextStyle: {
+    textAlign: 'center',
+    fontSize: FONT_SIZE,
+    color: HIGHLIGHT_COLOR
+  },
 
-    sectionStyle: {
-        padding: PADDING * 2,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
+  sectionStyle: {
+    padding: PADDING * 2,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
 
-    sectionTextStyle: {
-        textAlign: 'center',
-        fontSize: FONT_SIZE
-    }
+  sectionTextStyle: {
+    textAlign: 'center',
+    fontSize: FONT_SIZE
+  }
 });
